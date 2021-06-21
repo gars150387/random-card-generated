781628da3f53c05fd2b67e7feec27fd9f29007e4
gars150387 <gar.santeliz@gmail.com>
2021-06-21 23:01:16 +0000

    origin

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 function number() {
     let numRandom = Math.floor(Math.random() * 13 + 1);
-    if(numRandom > 10 && numRandom<12){
+    if (numRandom > 10 && numRandom < 12) {
         return 'J';
-    }else if(numRandom>11 && numRandom<13){
+    } else if (numRandom > 11 && numRandom < 13) {
         return 'Q';
-    }else if(numRandom>12){
+    } else if (numRandom > 12) {
         return 'K';
-    }else{
+    } else {
         return numRandom;
     }
-    }
+}
 
 document.getElementById("demo").innerHTML = number();
 
 function suit() {
     let random = Math.floor(Math.random() * 4 + 1);
-    switch (random){
+    switch (random) {
         case 1:
             return '♦'
         case 2:
@@ -25,7 +25,21 @@ function suit() {
         case 4:
             return '♣'
     }
+
 }
-let suit1 = suit()
-document.getElementById('suit').innerHTML=suit1;
-document.getElementById('suit2').innerHTML=suit1;
+
+function suitColor(suit) {
+    let red = suit
+    if (suit === '♦' || suit === '♥') {
+        // document.getElementById('red').style.color = 'red';
+        // return red;
+        document.getElementById('suit').style.color='red';
+        document.getElementById('suit2').style.color='red';
+
+    }
+}
+
+    let suit1 = suit()
+    suitColor(suit1)
+    document.getElementById('suit').innerHTML = suit1;
+    document.getElementById('suit2').innerHTML = suit1;
